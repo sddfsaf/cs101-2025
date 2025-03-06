a8a422ed8d5f613914e1ac72e47d263a81b02c6b
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2025-03-06 13:22:56 +0800

    Update main.c

--- ex1/main.c
+++ ex1/main.c
@@ -1,1 +1,23 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   int a[] ={1, 2, 3};
+   int ar[3];
+   char b[] = "ABC";
+   float c[] = {1.1, 1.2, 1.3};
+   FILE* fp;
+   
+   fp = fopen("a.bin", "wb+");
+   fwrite(a, sizeof(a), 1, fp);
+   
+   fread(ar, sizeof(a), 1, fp);
+   for( int i =0;i <3; i++){
+       printf("%d ", ar[i]);
+   }
+   fp = fclose("a.bin");
+
+    return 0;
+}
