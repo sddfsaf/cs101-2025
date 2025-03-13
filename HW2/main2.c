aa43f9d227a022f6db041145204df707407bdba7
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2025-03-13 14:18:41 +0800

    Create main2.c

---
+++ HW2/main2.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    FILE *fp;
+    FILE * fpr;
+    char m_read[1000];
+    fp = fopen("main2.txt", "w+");
+    fpr = fopen("main.c", "r");
+    fread(m_read, sizeof(m_read), 1, fpr);
+    fprintf(fp, "%s", m_read);
+    fclose(fp);
+
+    return 0;
+}
