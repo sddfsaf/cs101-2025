53f645b413a1217589b5a687ba24ab4cda455e3a
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2025-03-13 14:15:58 +0800

    Create maiun1.c

---
+++ HW2/maiun1.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main()
+{
+    int n =7;
+    int l;
+    printf("請輸入要的樂透份數：");
+    scanf("%d", &l);
+    int cout[n]; 
+    FILE* fp;
+    fp = fopen("lotto.txt", "w");
+    time_t curtime;
+    time(&curtime);
+    srand(2025);
+    fprintf(fp, "%s", ctime(&curtime));
+    for (int i =1; i <=l; i++){
+        for ( int k =0;k <n; k++){
+            
+            cout[k] = rand() %69 + 1;
+                for ( int j =0;j <k; j++){
+                    if (cout[k] == cout[j]){
+                        k--;
+                        break;
+                    }
+                }
+                
+        }
+        fprintf(fp, "[%d]: ", i);
+        for(int a =0; a <n; a++){
+            fprintf(fp, "%d ", cout[a]);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose (fp);
+    return 0;
+}
