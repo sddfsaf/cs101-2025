628ac71eea07718939321e6d813be8258f69659b
sddfsaf <120005665+sddfsaf@users.noreply.github.com>
2025-03-27 13:31:53 +0800

    Create Main30.c

---
+++ ex4/Main30.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char* get_int_array(int n){
+    return (char *) malloc (n* sizeof (char));
+}
+
+int main()
+{
+    int size = 8;
+    char* ptr = get_int_array(size);
+    
+    strcpy(ptr, "IU12345");
+    printf("%s\n",ptr);
+    strcpy(ptr, "IU123456789ABCDEFG");
+    printf("%s\n",ptr);
+    free(ptr);
+    ptr = NULL;
+    
+
+    return 0;
+}
